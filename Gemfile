74ee771f3bbf9b9d55313d83843fe92f6a232053
Herinson Rodrigues <herinson.br@gmail.com>
2016-12-30 02:19:17 -0300

    Fix several minor layout issues

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ gem 'jekyll'
 
 group :jekyll_plugins do
   gem "jekyll-paginate"
-  gem "jekyll-polyglot"
   gem "html-proofer"
   gem "kramdown"
 end
--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,11 @@
     $sidebarStyles += ' min-height: ' + $(document).height() + 'px;';
     $('.sidebar').attr('style', $sidebarStyles);
 
-    /* Secondary contact links */
-    var $scontacts = $('#contact-list-secondary');
-    var $contactList = $('#contact-list');
-
-    $scontacts.hide();
-    $contactList.mouseenter(function(){ $scontacts.fadeIn(); });
-    $contactList.mouseleave(function(){ $scontacts.fadeOut(); });
-
     /**
      * Tags & categories tab activation based on hash value. If hash is undefined then first tab is activated.
      */
     function activateTab() {
-      if(['/tags.html', '/categories.html'].indexOf(window.location.pathname) > -1) {
+      if(['/tags', '/categories'].indexOf(window.location.pathname) > -1) {
         var hash = window.location.hash;
         if(hash)
           $('.tab-pane').length && $('a[href="' + hash + '"]').tab('show');
